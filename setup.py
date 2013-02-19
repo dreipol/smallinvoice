69f1a7674d7d67788a38ebeb449045be64a707a7
phil <philipp.laeubli@dreipol.ch>
2013-02-19 13:10:59 +0100

    more reformatting

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ __author__ = 'phil'
 from distutils.core import setup
 
 setup(
-	name='smallinvoice',
-	version='0.1.1',
-	author='dreipol GmbH: Philipp Laeubli, Etienne Wild',
-	author_email='dev@dreipol.ch',
-	packages=['smallinvoice', 'smallinvoice.tests'],
-	url='http://pypi.python.org/pypi/Smallinvoice/',
-	license='LICENSE.txt',
-	description='A simple client wrapper for the smallinvoice.ch web api',
-	long_description=open('README.txt').read(),
-	install_requires=[
-		"requests",
-		"jsonpickle",
-		],
+    name='smallinvoice',
+    version='0.1.1',
+    author='dreipol GmbH: Philipp Laeubli, Etienne Wild',
+    author_email='dev@dreipol.ch',
+    packages=['smallinvoice', 'smallinvoice.tests'],
+    url='http://pypi.python.org/pypi/Smallinvoice/',
+    license='LICENSE.txt',
+    description='A simple client wrapper for the smallinvoice.ch web api',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests",
+        "jsonpickle",
+    ],
 )
