83dfde20b8d621c084642bab655e06b9c61aa919
phil <phil@dreipol.ch>
2013-06-26 19:07:06 +0200

    language code must be lowercase

--- smallinvoice/tests/clients_test.py
+++ smallinvoice/tests/clients_test.py
@@ -19,7 +19,7 @@ def test_add_address():
     a = Address(primary=1, street="Kernstrasse", streetno="60", city="Zurich",
                 code="8004", country="CH")
     c = Customer(type=CUSTOMER_TYPE.PRIVATE, gender=CUSTOMER_GENDER.MALE,
-                 name="Hanspeter Muster", language="DE", addresses=[a])
+                 name="Hanspeter Muster", language="de", addresses=[a])
     client = Client(TEST_API_TOKEN)
     client_id = client.clients.add(c)
     details = client.clients.details(client_id)
@@ -32,7 +32,7 @@ def test_update_client():
                 code="8004", country="CH")
     a.id = 50341
     c = Customer(type=CUSTOMER_TYPE.PRIVATE, gender=CUSTOMER_GENDER.MALE,
-                 name="Hans Muster", language="DE", addresses=[a])
+                 name="Hans Muster", language="de", addresses=[a])
     c.id = 49677
     client = Client(TEST_API_TOKEN)
     client.clients.update(c.id, c)
